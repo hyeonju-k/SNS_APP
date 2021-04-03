2218228d5f8c422941beb0309815f438d1fbd0d5
HyeonJu <hj950000@ajou.ac.kr>
2021-04-03 19:21:25 +0900

    Add image and video posting

--- app/src/main/java/com/example/community/activity/GalleryActivity.java
+++ app/src/main/java/com/example/community/activity/GalleryActivity.java
@@ -1,6 +1,7 @@
 package com.example.community.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -41,23 +42,28 @@ public class GalleryActivity extends BasicActivity {
         recyclerView.setAdapter(mAdapter);
     }
 
-    public static ArrayList<String> getImagesPath(Activity activity) {
+    public ArrayList<String> getImagesPath(Activity activity) {
         Uri uri;
         ArrayList<String> listOfAllImages = new ArrayList<String>();
         Cursor cursor;
-        int column_index_data, column_index_folder_name;
+        int column_index_data;
         String PathOfImage = null;
-        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection;
+        Intent intent = getIntent();
 
-        String[] projection = { MediaStore.MediaColumns.DATA,
-                MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+        if(intent.getStringExtra("media").equals("video")){
+            uri = android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+            projection = new String[]{ MediaStore.MediaColumns.DATA, MediaStore.Video.Media.BUCKET_DISPLAY_NAME };
+        }else{
+            uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            projection = new String[]{ MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+        }
 
         cursor = activity.getContentResolver().query(uri, projection, null,
                 null, null);
 
         column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-        column_index_folder_name = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+
         while (cursor.moveToNext()) {
             PathOfImage = ((Cursor) cursor).getString(column_index_data);
 
--- app/src/main/java/com/example/community/activity/WritePostActivity.java
+++ app/src/main/java/com/example/community/activity/WritePostActivity.java
@@ -1,63 +1,175 @@
 package com.example.community.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.bumptech.glide.Glide;
 import com.example.community.R;
+import com.example.community.view.MemberInfo;
 import com.example.community.view.WriteInfo;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class WritePostActivity extends BasicActivity {
 
     private static final String TAG = "WritePostActivity";
     private FirebaseUser user;
+    private ArrayList<String> pathList = new ArrayList<>();
+    private LinearLayout parent;
+    private int pathCount, successCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_post);
 
+        parent = findViewById(R.id.contentsLayout);
 
         findViewById(R.id.check).setOnClickListener(onClickListener);
+        findViewById(R.id.image).setOnClickListener(onClickListener);
+        findViewById(R.id.video).setOnClickListener(onClickListener);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 0: {
+                if (resultCode == Activity.RESULT_OK) {
+                    String profilePath = data.getStringExtra("profilePath");
+                    pathList.add(profilePath);
+
+                    ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    ImageView imageView = new ImageView(WritePostActivity.this);
+                    imageView.setLayoutParams(layoutParams);
+                    Glide.with(this).load(profilePath).override(1000).into(imageView);
+                    parent.addView(imageView);
+
+                    EditText editText = new EditText(WritePostActivity.this);
+                    editText.setLayoutParams(layoutParams);
+                    editText.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE | InputType.TYPE_CLASS_TEXT);
+                    parent.addView(editText);
+                }
+                break;
+            }
+        }
+    }
 
     View.OnClickListener onClickListener = new View.OnClickListener(){
         @Override
         public void onClick(View v){
             switch (v.getId()){
                 case R.id.check :
-                    postUpload();
+                    storageUpload();
+                    break;
+                case R.id.image :
+                    myStartActivity(GalleryActivity.class, "image");
+                    break;
+                case R.id.video :
+                    myStartActivity(GalleryActivity.class, "video");
                     break;
             }
         }
     };
 
-    public void postUpload(){
+    public void storageUpload(){
         final String title = ((EditText) findViewById(R.id.titleEditText)).getText().toString();
-        final String contents = ((EditText) findViewById(R.id.contentsEditText)).getText().toString();
 
-        if (title.length() > 0 && contents.length() > 0) {
+        if (title.length() > 0) {
+            final ArrayList<String> contentsList = new ArrayList<>();
+
             user = FirebaseAuth.getInstance().getCurrentUser();
-            WriteInfo writeInfo= new WriteInfo(title, contents, user.getUid());
-            uploader(writeInfo);
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+
+            for(int i=0; i<parent.getChildCount(); i++){
+                View view = parent.getChildAt(i);
+                if(view instanceof EditText){
+                    String text = ((EditText)view).getText().toString();
+                    if(text.length()>0){
+                    contentsList.add(text);
+                    }
+                }else{      // imageView
+                    contentsList.add(pathList.get(pathCount));
+
+                    final StorageReference mountainImagesRef = storageRef.child("users/" + user.getUid() + "/"+ pathCount+".jpg");
+
+                    try {
+                        InputStream stream = new FileInputStream(new File(pathList.get(pathCount)));
+
+                        //for indexing
+                        StorageMetadata metadata = new StorageMetadata.Builder().setCustomMetadata("index", ""+(contentsList.size()-1)).build();
+                        UploadTask uploadTask = mountainImagesRef.putStream(stream, metadata);
+                        uploadTask.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Handle unsuccessful uploads
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                final int index = Integer.parseInt(taskSnapshot.getMetadata().getCustomMetadata("index"));
+
+                                mountainImagesRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        contentsList.set(index, uri.toString());
+                                        successCount++;
+                                        if(pathList.size() == successCount){
+                                            // 완료
+                                            WriteInfo writeInfo= new WriteInfo(title, contentsList, user.getUid(), new Date());
+                                            storeUploader(writeInfo);
+                                        }
+                                    }
+                                });
+
+                                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                                // ...
+                            }
+                        });
+                    } catch (FileNotFoundException e) {
+                        Log.e("로그", "에러:" + e.toString());
+                    }
+                    pathCount++;
+                }
+            }
         }
         else {
             startToast("회원정보를 입력해주세요");
         }
     };
 
-    private void uploader(WriteInfo writeInfo){
+    private void storeUploader(WriteInfo writeInfo){
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("posts").add(writeInfo)
@@ -65,6 +177,7 @@ public class WritePostActivity extends BasicActivity {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
                        Log.d(TAG, "DocumentSnapshot written with ID:"+ documentReference.getId());
+                       finish();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -77,5 +190,11 @@ public class WritePostActivity extends BasicActivity {
 
     private void startToast(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void myStartActivity(Class c, String media){
+        Intent intent = new Intent(this,c);
+        intent.putExtra("media", media);
+        startActivityForResult(intent,0);
     }
 }
